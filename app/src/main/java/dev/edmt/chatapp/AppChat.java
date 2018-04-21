6d1623a89beea6a79dec08dae139147a32871f52
berliantosaragih <35856411+berliantosaragih@users.noreply.github.com>
2018-04-21 10:25:33 +0700

    yee
    yee

---
+++ app/src/main/java/dev/edmt/chatapp/AppChat.java
@@ -0,0 +1,47 @@
+package dev.edmt.chatapp;
+
+import java.util.Date;
+
+/**
+ * Created by reale on 18/11/2016.
+ */
+
+public class AppChat {
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public AppChat(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        messageTime = new Date().getTime();
+    }
+
+    public AppChat() {
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
